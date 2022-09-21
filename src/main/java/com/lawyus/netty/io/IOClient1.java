0fcbf76a5893506671bcfb291b4b2ea7ab4d7444
yushing <luoyushing@gmail.com>
2022-09-21 18:07:59 +0800

    socket IO模型

---
+++ src/main/java/com/lawyus/netty/io/IOClient1.java
@@ -0,0 +1,32 @@
+package com.lawyus.netty.io;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+public class IOClient1 {
+
+    /**
+     * Client客户端连接服务端8000端口每隔2秒向服务端写带有时间戳的 "hello world"
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world, " + socket.getLocalPort()).getBytes());
+                        socket.getOutputStream().flush();
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
---
+++ src/main/java/com/lawyus/netty/io/IOServer.java
@@ -0,0 +1,43 @@
+package com.lawyus.netty.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class IOServer {
+
+    /**
+     * Server服务端首先创建ServerSocket监听8000端口,然后不断调用阻塞方法 serversocket.accept()获取新的连接,
+     * 当获取到新的连接给每条连接创建新的线程负责从该连接中读取数据,然后读取数据是以字节流的方式
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8000);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+
+                // 新建线程接收数据
+                new Thread(() -> {
+                    try {
+                        InputStream input = socket.getInputStream();
+                        byte[] data = new byte[1024];
+                        int length;
+                        while ((length = input.read(data)) != -1) {
+                            System.out.println(new String(data, 0, length));
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+
